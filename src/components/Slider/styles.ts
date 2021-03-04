3d0d352eb6002f4b0596c49c58aa73980beade2d
Gabriel <gabrieloureirof@gmail.com>
2021-03-04 10:22:39 -0300

    fix z-index slider

--- src/components/Slider/styles.ts
+++ src/components/Slider/styles.ts
@@ -6,12 +6,14 @@ export const CustomSlider = styled(Slider)`
   margin: 24px auto;
   width: calc(100% - 64px);
   max-width: 1180px;
+  z-index: 2;
 `
 
 export const Wrapper = styled(motion.div)`
   width: 100%;
   padding: 16px 0 24px 0;
   display: flex;
+  z-index: 2;
 
   .slick-track {
     padding: 12px 0;
