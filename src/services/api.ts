c062f8d991e0aa579b303cf97deaa31317afe0c9
Gabriel <gabrieloureirof@gmail.com>
2021-03-04 02:32:56 -0300

    fix api to connect

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_FLAPPER,
+  baseURL: 'https://flapper-frontend-challenge.herokuapp.com/',
   responseType: 'json',
   headers: {
     'content-type': 'application/json',
